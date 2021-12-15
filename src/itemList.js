170ffb718e54ddb7a8cf3c602c9fe3124207b7a0
Rodrigo <rodsan05@ucm.es>
2021-12-15 19:08:01 +0100

    lista no funciona pero no da errores

--- src/itemList.js
+++ src/itemList.js
@@ -4,7 +4,7 @@
  * Clase que representa la lista de objetos
 */
 export default class ItemList extends Phaser.GameObjects.Sprite {
-  
+
     /**
      * Constructor de la barra
      * @param {Phaser.Scene} scene Escena a la que pertenece la barra
@@ -20,67 +20,52 @@ export default class ItemList extends Phaser.GameObjects.Sprite {
 
         //this.sprite = new Phaser.GameObjects.Sprite(scene, 0, 0, 'itemlist');
         this.text = new Phaser.GameObjects.Text(scene, 0, 0, ' ');
-        
+
         this.displayWidth = _width;
         this.displayHeight = _height;
 
+        this.hiddenX = hx;
+        this.shownX = sx
+
         this.setDepth(10);
-        //this.setOrigin(0, 0.5);
 
-
-        //this.scene.add(this.sprite);
-        //this.scene.add(this.text);
-        //this.scene.add.existing(this);
-
-        
-
-        
         this.opened = false;
 
-        this.items = {tablas: 0, cruz: false, pala: false, sierra: false, clavos: false, martillo: false, bisagras: false};
-        
-        this.show = this.scene.tweens.add({
-            targets: this,
-            x: sx,
-            ease: 'Back',
-            easeParams: [1.5],
-            delay: 500
-            })
-        this.hide = this.scene.tweens.add({
-            targets: this,
-            x: hx,
-            ease: 'Back',
-            easeParams: [1.5],
-            delay: 500
-        })
+        this.items = { tablas: 0, cruz: false, pala: false, sierra: false, clavos: false, martillo: false, bisagras: false };
 
-        let input = this.scene.input.keyboard.addKey('L');
+        this.input = this.scene.input.keyboard.addKey('L');
 
-        if (input.isDown)
-        {
-            console.log("Tab pressed!");
-            this.opened = !this.opened;
-            if (this.opened){
-                
-                this.show.restart();
-            }
-            else{
-                this.hide.on;
-            }
-        }
+        this.input.on('down', this.ShowItemList, this);
+        this.input.on('up', this.ShowItemList, this);
     }
 
-    ShowItemList()
-    {
-        this.show.resume();
-        this.opened = !this.opened;
-            if (this.opened){
-                console.log("opening menu");
-                //this.show.restart();
-            }
-            else{
-                console.log("closing menu");
-                //this.hide.restart();
-            }
+    ShowItemList() {
+
+        let show = this.scene.tweens.add({
+            targets: [this],
+            x: this.shownX,
+            duration: 100,
+            ease: 'Back.easeOut',
+            paused: false
+        });
+
+        this.opened = true;
+
+        if (this.scene.DEBUG) console.log("opening menu");
+    }
+
+    HideItemList() {
+
+        let hide = this.scene.tweens.add({
+            targets: [this],
+            x: this.hiddenX,
+            duration: 100,
+            ease: 'Back.easeOut',
+            paused: false
+        });
+
+        this.opened = false;
+
+        if (this.scene.DEBUG) console.log("closing menu");
     }
 }   
